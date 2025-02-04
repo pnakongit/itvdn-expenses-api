065be6718f4f205d4ea49b580f7d8aacbf50be88
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-02-04 17:26:21 +0200

    Integrate SQLAlchemy with Flask application

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+DATABASE_URI = "sqlite:///expenses.sqlite3"
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
 @app.route('/')
@@ -9,4 +21,7 @@ def index() -> (dict, 200):
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
