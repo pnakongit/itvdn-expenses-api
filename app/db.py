69ed9541e74b381799b9c309ace4965dd48f98ec
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-02-14 19:06:36 +0200

    Add naming_convention to metadata in Base model

--- app/db.py
+++ app/db.py
@@ -3,7 +3,15 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
 
 db = SQLAlchemy(model_class=Base)
