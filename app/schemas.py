20c4306ff3e51c3c1a67bfc8dbe8298a3a538d90
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-02-17 20:04:37 +0200

    Simplify UserSchema by inheriting from UserSchemaLogin

--- app/schemas.py
+++ app/schemas.py
@@ -24,10 +24,7 @@ class UserSchemaLogin(Schema):
     password = fields.Str(load_only=True, required=True, validate=validate.Length(min=4))
 
 
-class UserSchema(Schema):
-    id = fields.Integer(dump_only=True)
-    username = fields.Str(required=True, validate=validate.Length(min=4, max=20))
-    password = fields.Str(load_only=True, required=True, validate=validate.Length(min=4))
+class UserSchema(UserSchemaLogin):
 
     @validates("username")
     def validate_username(self, value: str, **kwargs) -> None:
