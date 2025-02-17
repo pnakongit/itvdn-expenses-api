6c6a8fe6d23c6b19381d320302c97cad08f3adb4
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-02-17 18:01:18 +0200

    Add handle_unauthorized to error_handlers

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -1,5 +1,5 @@
 from flask import Response, jsonify
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import NotFound, Unauthorized
 from marshmallow import ValidationError
 
 
@@ -16,3 +16,14 @@ def handle_not_fount(e: NotFound) -> (Response, int):
 
 def handle_schema_errors(e: ValidationError) -> (Response, int):
     return jsonify(errors=e.messages_dict), 400
+
+
+def handle_unauthorized(e: Unauthorized) -> (Response, int):
+    data = {
+        "error": {
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        }
+    }
+    return jsonify(data), e.code
