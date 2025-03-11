7a14226fe08cf6797dad8b24d70d5ef1ced0fac2
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-03-11 22:46:22 +0200

    Use values from environment variables

--- app/config.py
+++ app/config.py
@@ -1,12 +1,14 @@
 import datetime
+import os
 
 
 class BaseConfig:
+
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///expenses.sqlite3"
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SPEC_URL = "/spec"
     BASE_SWAGGER_URL = "/swagger"
-    JWT_SECRET_KEY = "Test JWT"
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(hours=1)
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=5)
 
